f234b26906ade64dbfb468a52a34ce07e335dda5
Laura <lauraalvarez0800@gmail.com>
2022-04-19 19:16:03 +0200

    golay

---
+++ codigogolay.py
@@ -0,0 +1,100 @@
+#### CODIGO HAMMING
+option=int(input('1- Generar un código hamming  \n2- Eencontrar error en código hamming \n--- Selecciona la opción: ---\n'))
+
+if(option==1):  # GENERAR CODIGO HAMMING 
+    print('Introduce los bits de datos:')
+    d=input()
+    data=list(d)
+    data.reverse()
+    c,ch,j,r,h=0,0,0,0,[]
+
+    while ((len(d)+r+1)>(pow(2,r))):
+        r=r+1
+
+    for i in range(0,(r+len(data))):
+        p=(2**c)
+
+        if(p==(i+1)):
+            h.append(0)
+            c=c+1
+
+        else:
+            h.append(int(data[j]))
+            j=j+1
+
+    for parity in range(0,(len(h))):
+        ph=(2**ch)
+        if(ph==(parity+1)):
+            startIndex=ph-1
+            i=startIndex
+            toXor=[]
+
+            while(i<len(h)):
+                block=h[i:i+ph]
+                toXor.extend(block)
+                i+=2*ph
+
+            for z in range(1,len(toXor)):
+                h[startIndex]=h[startIndex]^toXor[z]
+            ch+=1
+
+    h.reverse()
+    print('EL código Hamming generado es:- ', end="")
+    print(int(''.join(map(str, h))))
+
+
+elif(option==2): # DETECTAR ERROR EN CODIGO HAMMING
+    print('Introduce el código Hamming recibido:')
+    d=input()
+    data=list(d)
+    data.reverse()
+    c,ch,j,r,error,h,parity_list,h_copy=0,0,0,0,0,[],[],[]
+
+    for k in range(0,len(data)):
+        p=(2**c)
+        h.append(int(data[k]))
+        h_copy.append(data[k])
+        if(p==(k+1)):
+            c=c+1
+            
+    for parity in range(0,(len(h))):
+        ph=(2**ch)
+        if(ph==(parity+1)):
+
+            startIndex=ph-1
+            i=startIndex
+            toXor=[]
+
+            while(i<len(h)):
+                block=h[i:i+ph]
+                toXor.extend(block)
+                i+=2*ph
+
+            for z in range(1,len(toXor)):
+                h[startIndex]=h[startIndex]^toXor[z]
+            parity_list.append(h[parity])
+            ch+=1
+    parity_list.reverse()
+    error=sum(int(parity_list) * (2 ** i) for i, parity_list in enumerate(parity_list[::-1]))
+    
+    if((error)==0):
+        print('No hay ningún error en el código Hamming recivido')
+
+    elif((error)>=len(h_copy)):
+        print('El error no se puede detectar')
+
+    else:
+        print('Error en el ',error,' bit')
+
+        if(h_copy[error-1]=='0'):
+            h_copy[error-1]='1'
+
+        elif(h_copy[error-1]=='1'):
+            h_copy[error-1]='0'
+            print('El código Hamming corregido es:- ')
+        h_copy.reverse()
+        print(int(''.join(map(str, h_copy))))
+
+else:
+    print('La opcion seleccionada no existe')
+
